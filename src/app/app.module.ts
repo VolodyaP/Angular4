8f3f32e3862a1322e56cd911de1a47caef3422fe
VolodyaP <weber.uk.lv@gmail.com>
2017-09-11 21:41:39 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { FilterComponent } from './filter/filter.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    FilterComponent,
+    EventListComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule
